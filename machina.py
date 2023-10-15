7e164618175ffb18935588b37380a4e5af0f6282
Alex Fedotov <alxfed@gmail.com>
2023-10-15 10:23:44 -0500

    one

--- machina.py
+++ machina.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-0
-import curses
+import urwid
 
 if __name__=="__main__":
-    stdscr = curses.initscr()
-    stdscr.clear()
+    def exit_on_q(key):
+        if key in ('q', 'Q'):
+            raise urwid.ExitMainLoop()
+
+    palette = [
+        ('banner', 'black', 'light gray'),
+        ('streak', 'black', 'red'),
+        ('bg', 'black', 'blue'), ]
+
+    txt = urwid.Text(('banner', u" Hello World "), align='center')
+    map1 = urwid.AttrMap(txt, 'streak')
+    fill = urwid.Filler(map1)
+    map2 = urwid.AttrMap(fill, 'bg')
+    loop = urwid.MainLoop(map2, palette, unhandled_input=exit_on_q)
+    loop.run()
     print("Hi, I'm Machina Ratiocinatrix.")
 
 """or
